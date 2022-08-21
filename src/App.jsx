24b5642eef29d43d2756d2782ea6268ae90422aa
Nahuel-DevOne <nahuel.profesor@gmail.com>
2022-08-20 21:35:32 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+
+    </div>
+  )
+}
+
+export default App
